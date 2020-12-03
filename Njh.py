d8a404163ea691b46f70425b57469bf04f6e6088
Sina Kamali <sininoir@gmail.com>
2020-12-03 19:34:32 +0330

    completed calculate conditional P

--- Njh.py
+++ Njh.py
@@ -2,6 +2,7 @@ import os
 from time import time
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 class Njh:
     def __init__(self, corpus_path, tuple_list=None, alpha=1, window_size=[1,0]):
@@ -21,7 +22,9 @@ class Njh:
         corpus_file = open(corpus_path)
         self.corpus = corpus_file.read().split()
         self.uniques, self.uniques_count = list(np.unique(self.corpus, return_counts = True))
+        print('doing preprocessing on corpus...')
         self._make_context_holder()
+        print('preprocessing is done.')
         self._fill_ws_dict()
 
     def _make_context_holder(self):
@@ -44,7 +47,7 @@ class Njh:
         context_ref = self.context_holder[cols]
         records = context_ref.to_records(index=False)
         all_contexts = np.array(records)
-        self.unique_contexts, self.unique_contexts_count = np.unique(all_contexts, return_counts = True)
+        self.unique_contexts, self.unique_contexts_count = (np.unique(all_contexts, return_counts = True))
 
     def _fill_ws_dict(self):
         self.ws_dict = {}
@@ -55,28 +58,32 @@ class Njh:
             for word_index in range(len(self.uniques)):
                 word = self.uniques[word_index]
                 word_dict = {}
-                for second_word_index in range(len(self.uniques)):
+                for second_word_index in tqdm(range(len(self.uniques))):
                     second_word = self.uniques[second_word_index]
                     word_dict[second_word] = self._calculate_similarity(word_index, second_word_index)
                 self.ws_dict[word] = word_dict
 
-    def _calculate_N(self, w1, context):
-        #TODO: code here
+    def _calculate_N(self, w1, context_index):
+        """Calculates N(w1|context)"""
         center = self.window_size[0]
-        context_df = self.context_holder[self.context_holder[self.context_holder.columns[center]] == context]
+        needed_row = list(self.unique_contexts[context_index])
+        needed_row.insert(center, w1)
+        N = (self.context_holder == needed_row).all(1).sum()
+        return N
 
-    def _calculate_conditional_P(self, w1, w2):
+    def _calculate_conditional_P(self, w1, context_index):
         #TODO: code here
-        """Calculates P(w1|w2)"""
-        self._calculate_N(w1, w2)
-        
+        """Calculates P(w1|context)"""
+        N_w1_context = self._calculate_N(w1, context_index)
+        N_context = self.unique_contexts_count[context_index]
+        return (N_w1_context / N_context)
 
     def _calculate_similarity(self, w1_index, w2_index):
         #TODO: code here
         """Calculates S(w1,w2)"""
-        self._calculate_conditional_P(self.uniques[w1_index], self.uniques[w2_index])
+        self._calculate_conditional_P(self.uniques[w1_index], 800)
         
 
 x = time()
-Njh('./corpus.txt', window_size=[2,1])
+Njh('./corpus.txt', window_size=[1,1])
 print(f'{time() - x} seconds')
